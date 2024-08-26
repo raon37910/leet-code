6dd9d2def9db60ce456910496b7450f6339a5317
raon37910 <raon37910@gmail.com>
2024-08-26 19:31:33 +0900

    Time: 73 ms (48.34%), Space: 52.3 MB (64.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,10 +1,14 @@
+type obj = {
+    [key: number]: number;
+}
+
 function twoSum(nums: number[], target: number): number[] {
-    for(let i =0; i < nums.length-1; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+    const hMap:obj = {};
+    for (let i =0; i<nums.length; i++) {
+        if (target-nums[i] in hMap) {
+            return [hMap[target-nums[i]], i];
         }
+        hMap[nums[i]] = i;
     }
-    return []
+    return [-1, -1];
 };
